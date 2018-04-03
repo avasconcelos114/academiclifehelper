049b23f1f5e4c8e8f1b91c00d8141ff75f63d205
Unknown <andre.onogoro@gmail.com>
2018-04-03 17:47:57 +0900

    Added basic layout & contents to Home screen

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,10 +1,14 @@
 import angular from 'angular';
 import Home from './home/home';
 import About from './about/about';
+import CompanyInfo from './companyInfo/companyInfo'
+import Contribute from './contribute/contribute'
 
 let componentModule = angular.module('app.components', [
   Home,
-  About
+  About,
+  CompanyInfo,
+  Contribute
 ])
 
 .name;
